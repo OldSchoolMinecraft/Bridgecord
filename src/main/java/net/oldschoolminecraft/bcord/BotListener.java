353d62d8667197bb658bfda894dfa596645a608c
moderatorman <12193288+moderatorman@users.noreply.github.com>
2023-12-28 19:24:33 -0800

    async bot listener

--- src/main/java/net/oldschoolminecraft/bcord/BotListener.java
+++ src/main/java/net/oldschoolminecraft/bcord/BotListener.java
@@ -36,156 +36,181 @@ public class BotListener extends ListenerAdapter
 
         if (listConf.isEnabled() && event.getMessage().getContentStripped().equalsIgnoreCase(cmdPrefix + listConf.getLabel()))
         {
-            if (Bukkit.getOnlinePlayers().length == 0)
+            asyncImmediately(() ->
             {
-                event.getMessage().reply("Nobody is online :(").queue();
-                return;
-            }
+                if (Bukkit.getOnlinePlayers().length == 0)
+                {
+                    event.getMessage().reply("Nobody is online :(").queue();
+                    return;
+                }
 
-            String hideWithPerm = String.valueOf(config.getConfigOption("hidePlayersWithPermission"));
-            Invisiman invisiman = (Invisiman) Bukkit.getServer().getPluginManager().getPlugin("Invisiman");
-            boolean useInvisiman = (boolean) config.getConfigOption("useInvisiman");
-            boolean invisimanInstalled = invisiman != null;
-            StringBuilder sb = new StringBuilder();
-            int invisSub = 0;
-            for (Player p : Bukkit.getOnlinePlayers())
-            {
-                boolean playerIsVanished = (!useInvisiman && p.hasPermission(hideWithPerm)) || (useInvisiman && invisimanInstalled && invisiman.isVanished(p));
-                if (!playerIsVanished) sb.append(p.getName()).append(", ");
-                else invisSub++;
-            }
-            if (Bukkit.getOnlinePlayers().length - invisSub < 1)
-            {
-                event.getMessage().reply("Nobody is online :(").queue();
-                return;
-            }
-            String pre = sb.toString().trim();
-            event.getMessage().reply("Online players (" + Math.max(Bukkit.getOnlinePlayers().length - invisSub, 0) + "):\n`" + pre.substring(0, pre.length() - 1) + "`").queue();
+                String hideWithPerm = String.valueOf(config.getConfigOption("hidePlayersWithPermission"));
+                Invisiman invisiman = (Invisiman) Bukkit.getServer().getPluginManager().getPlugin("Invisiman");
+                boolean useInvisiman = (boolean) config.getConfigOption("useInvisiman");
+                boolean invisimanInstalled = invisiman != null;
+                StringBuilder sb = new StringBuilder();
+                int invisSub = 0;
+                for (Player p : Bukkit.getOnlinePlayers())
+                {
+                    boolean playerIsVanished = (!useInvisiman && p.hasPermission(hideWithPerm)) || (useInvisiman && invisimanInstalled && invisiman.isVanished(p));
+                    if (!playerIsVanished) sb.append(p.getName()).append(", ");
+                    else invisSub++;
+                }
+                if (Bukkit.getOnlinePlayers().length - invisSub < 1)
+                {
+                    event.getMessage().reply("Nobody is online :(").queue();
+                    return;
+                }
+                String pre = sb.toString().trim();
+                event.getMessage().reply("Online players (" + Math.max(Bukkit.getOnlinePlayers().length - invisSub, 0) + "):\n`" + pre.substring(0, pre.length() - 1) + "`").queue();
+            });
             return;
         }
 
         if (linkConf.isEnabled() && event.getMessage().getContentStripped().startsWith(cmdPrefix + linkConf.getLabel()))
         {
-            String[] args = event.getMessage().getContentStripped().split(" ");
-
-            if (args.length-1 < 1)
+            asyncImmediately(() ->
             {
-                event.getMessage().reply("You must specify a username! Example: `" + cmdPrefix + linkConf.getLabel() + " ExamplePlayer123`").queue();
-                return;
-            }
+                String[] args = event.getMessage().getContentStripped().split(" ");
 
-            String targetLinkName = args[1];
+                if (args.length-1 < 1)
+                {
+                    event.getMessage().reply("You must specify a username! Example: `" + cmdPrefix + linkConf.getLabel() + " ExamplePlayer123`").queue();
+                    return;
+                }
 
-            LinkData data = linkHandler.loadLinkDataByID(event.getAuthor().getId());
-            if (data != null)
-            {
-                event.getMessage().reply("You are already linked to `" + data.username + "` on this server :)").queue();
-                return;
-            }
+                String targetLinkName = args[1];
 
-            String code = linkHandler.startLinkProcess(targetLinkName, event.getAuthor().getId());
+                LinkData data = linkHandler.loadLinkDataByID(event.getAuthor().getId());
+                if (data != null)
+                {
+                    event.getMessage().reply("You are already linked to `" + data.username + "` on this server :)").queue();
+                    return;
+                }
 
-            event.getMessage().reply("Success! We have dispatched a Direct Message.").queue();
-            queueDM(event.getAuthor(), "You have successfully initiated the linking process! Please connect to the server with the username you provided (`" + targetLinkName + "`), and enter the following into the chat: `/dlink " + code + "`.\r\rIf the username shown in this message is not correct (and exact, it's case-sensitive!), please try linking again with the correct username.");
+                String code = linkHandler.startLinkProcess(targetLinkName, event.getAuthor().getId());
+
+                event.getMessage().reply("Success! We have dispatched a Direct Message.").queue();
+                queueDM(event.getAuthor(), "You have successfully initiated the linking process! Please connect to the server with the username you provided (`" + targetLinkName + "`), and enter the following into the chat: `/dlink " + code + "`.\r\rIf the username shown in this message is not correct (and exact, it's case-sensitive!), please try linking again with the correct username.");
+            });
             return;
         }
 
         if (resetConf.isEnabled() && event.getMessage().getContentStripped().equalsIgnoreCase(cmdPrefix + resetConf.getLabel()))
         {
-            LinkData data = linkHandler.loadLinkDataByID(event.getAuthor().getId());
-            if (data == null)
+            asyncImmediately(() ->
             {
-                event.getMessage().reply("Your account is not linked! Please use `" + cmdPrefix + linkConf.getLabel() + "` first!").queue();
-                return;
-            }
+                LinkData data = linkHandler.loadLinkDataByID(event.getAuthor().getId());
+                if (data == null)
+                {
+                    event.getMessage().reply("Your account is not linked! Please use `" + cmdPrefix + linkConf.getLabel() + "` first!").queue();
+                    return;
+                }
 
-            String newPassword = Util.generateSecurePassword(); // 12 char alphanumeric, no special characters
-            try
-            {
-                Util.selectAuthPlugin().updatePassword(data.username, newPassword);
-            } catch (AuthHandlerException ex) {
-                event.getMessage().reply("An unknown error has occurred while attempting to reset your password. Please try again later.").queue();
-                return;
-            }
+                String newPassword = Util.generateSecurePassword(); // 12 char alphanumeric, no special characters
+                try
+                {
+                    Util.selectAuthPlugin().updatePassword(data.username, newPassword);
+                } catch (AuthHandlerException ex) {
+                    event.getMessage().reply("An unknown error has occurred while attempting to reset your password. Please try again later.").queue();
+                    return;
+                }
 
-            event.getMessage().reply("Success! We have dispatched a Direct Message.").queue();
-            queueDM(event.getAuthor(), "Your in-game account has been updated with a new randomly generated password: `" + newPassword + "`. While we do not store these passwords, it is still recommended that you change it to something else. For added security, a password manager is encouraged (but not *required*).");
+                event.getMessage().reply("Success! We have dispatched a Direct Message.").queue();
+                queueDM(event.getAuthor(), "Your in-game account has been updated with a new randomly generated password: `" + newPassword + "`. While we do not store these passwords, it is still recommended that you change it to something else. For added security, a password manager is encouraged (but not *required*).");
+            });
             return;
         }
 
         if (authConf.isEnabled() && event.getMessage().getContentStripped().equalsIgnoreCase(cmdPrefix + authConf.getLabel()))
         {
-            LinkData data = linkHandler.loadLinkDataByID(event.getAuthor().getId());
-            if (data == null)
+            asyncImmediately(() ->
             {
-                event.getMessage().reply("Your account is not linked! Please use `" + cmdPrefix + linkConf.getLabel() + "` first!").queue();
-                return;
-            }
-
-            try
-            {
-                Player player = Bukkit.getPlayer(data.username);
-
-                if (player == null)
+                LinkData data = linkHandler.loadLinkDataByID(event.getAuthor().getId());
+                if (data == null)
                 {
-                    event.getMessage().reply("You must be connected to the server to use this feature!").queue();
+                    event.getMessage().reply("Your account is not linked! Please use `" + cmdPrefix + linkConf.getLabel() + "` first!").queue();
                     return;
                 }
 
-                String ip = ((CraftPlayer) player).getHandle().netServerHandler.networkManager.socket.getInetAddress().getHostAddress();
-                Util.selectAuthPlugin().authenticate(data.username, ip);
+                try
+                {
+                    Player player = Bukkit.getPlayer(data.username);
 
-                event.getMessage().reply("You have been successfully authorized in-game!").queue();
-            } catch (AuthHandlerException e) {
-                event.getMessage().reply("Error: " + e.getMessage()).queue();
-            }
+                    if (player == null)
+                    {
+                        event.getMessage().reply("You must be connected to the server to use this feature!").queue();
+                        return;
+                    }
+
+                    String ip = ((CraftPlayer) player).getHandle().netServerHandler.networkManager.socket.getInetAddress().getHostAddress();
+                    Util.selectAuthPlugin().authenticate(data.username, ip);
+
+                    event.getMessage().reply("You have been successfully authorized in-game!").queue();
+                } catch (AuthHandlerException e) {
+                    event.getMessage().reply("Error: " + e.getMessage()).queue();
+                }
+            });
 
             return;
         }
 
         if (setNameConf.isEnabled() && event.getMessage().getContentStripped().equalsIgnoreCase(cmdPrefix + setNameConf.getLabel()))
         {
-            boolean notImplemented = true;
-            if (notImplemented)
+            asyncImmediately(() ->
             {
-                event.getMessage().reply("This command is not yet implemented. Sorry!").queue();
-                return;
-            }
+                boolean notImplemented = true;
+                if (notImplemented)
+                {
+                    event.getMessage().reply("This command is not yet implemented. Sorry!").queue();
+                    return;
+                }
 
-            LinkData data = linkHandler.loadLinkDataByID(event.getAuthor().getId());
-            if (data == null)
-            {
-                event.getMessage().reply("Your account is not linked! Please use `" + cmdPrefix + linkConf.getLabel() + "` first!").queue();
-                return;
-            }
+                LinkData data = linkHandler.loadLinkDataByID(event.getAuthor().getId());
+                if (data == null)
+                {
+                    event.getMessage().reply("Your account is not linked! Please use `" + cmdPrefix + linkConf.getLabel() + "` first!").queue();
+                    return;
+                }
 
-            //TODO: create or update record with new display name
-            //TODO: cache data for 45 minutes in memory
+                //TODO: create or update record with new display name
+                //TODO: cache data for 45 minutes in memory
+            });
+
+            return;
         }
 
         List<String> channelIDs = config.getStringList("bridgeChannelIDs", Collections.emptyList());
         // read bridge messages from all configured channel IDs
         if (channelIDs.contains(event.getChannel().getId())) //if (event.getChannel().getId().equals(String.valueOf(config.getConfigOption("bridgeChannelID"))))
         {
-            ArrayList<String> msgChunks = new ArrayList<>();
+            asyncImmediately(() ->
+            {
+                ArrayList<String> msgChunks = new ArrayList<>();
 
-            //TODO: check if user has custom display name, and process accordingly -- check color role config & validate
+                //TODO: check if user has custom display name, and process accordingly -- check color role config & validate
 
-            String pre = Util.processMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(config.getConfigOption("bridgeMessageFormat.shownInGame"))), new HashMap<String, String>()
-            {{
-                put("{name}", event.getAuthor().getName());
-                put("{displayName}", event.getAuthor().getName());
-                put("{msg}", Util.stripAllColor(event.getMessage().getContentStripped()));
-            }});
-            if (pre.length() <= 128)
-                msgChunks.add(pre);
-            else {
-                msgChunks.addAll(Util.splitString(pre, 128));
-            }
-            for (Player p : Bukkit.getOnlinePlayers())
-                msgChunks.forEach(p::sendMessage);
-            return;
+                String pre = Util.processMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(config.getConfigOption("bridgeMessageFormat.shownInGame"))), new HashMap<String, String>()
+                {{
+                    put("{name}", event.getAuthor().getName());
+                    put("{displayName}", event.getAuthor().getName());
+                    put("{msg}", Util.stripAllColor(event.getMessage().getContentStripped()));
+                }});
+                if (pre.length() <= 128)
+                    msgChunks.add(pre);
+                else {
+                    msgChunks.addAll(Util.splitString(pre, 128));
+                }
+                for (Player p : Bukkit.getOnlinePlayers())
+                    msgChunks.forEach(p::sendMessage);
+            });
+//            return;
         }
+    }
+
+    private void asyncImmediately(Runnable runnable)
+    {
+        Bukkit.getScheduler().scheduleAsyncDelayedTask(Bridgecord.getInstance(), runnable, 0L);
     }
 
     private void queueDM(User user, String message)
