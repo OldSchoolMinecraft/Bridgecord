a128ef9f6b7463ab15d572b9a03c5be764535a1e
moderatorman <12193288+moderatorman@users.noreply.github.com>
2024-12-18 03:07:03 -0800

    Disable ENTITY_DEATH because its causing weird issues

--- src/main/java/net/oldschoolminecraft/bcord/Bridgecord.java
+++ src/main/java/net/oldschoolminecraft/bcord/Bridgecord.java
@@ -182,7 +182,8 @@ public class Bridgecord extends JavaPlugin
             getServer().getPluginManager().registerEvent(Event.Type.PLAYER_CHAT, currentEventHandler, eventPriority, this);
             getServer().getPluginManager().registerEvent(Event.Type.PLAYER_JOIN, currentEventHandler, eventPriority, this);
             getServer().getPluginManager().registerEvent(Event.Type.PLAYER_QUIT, currentEventHandler, eventPriority, this);
-            getServer().getPluginManager().registerEvent(Event.Type.ENTITY_DEATH, currentEventHandler, eventPriority, this);
+            //TODO: THIS NEEDS TO BE FIXED - it causes ClassCastException spam in the log
+//            getServer().getPluginManager().registerEvent(Event.Type.ENTITY_DEATH, currentEventHandler, eventPriority, this);
         }
     }
 
